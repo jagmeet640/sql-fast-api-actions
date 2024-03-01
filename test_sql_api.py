7e8979ecd1883a912e0d5cea512f24da052eb832
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2024-03-01 14:53:37 +0400

    Update test_sql_api.py

--- test_sql_api.py
+++ test_sql_api.py
@@ -12,7 +12,7 @@ def test_get_students():
 
 def test_create_student():
     # Send a POST request to create a student
-    response = requests.post("http://127.0.0.1:8000/students/", json={"name": "test", "age": 21, "marks": 95, "rollId":4})
+    response = requests.post("http://127.0.0.1:8000/students/", json={"name": "test", "age": 21, "marks": 95, "rollId":22})
     print(response.json())
     assert response.status_code == 200
 
@@ -20,7 +20,6 @@ def test_update_student():
     response = requests.put("http://127.0.0.1:8000/students_update/", json={"name": "aman", "age": 25, "marks": 100, "rollId": 22})
     print(response.json())
     assert response.status_code == 200
-
 
 def test_delete_student():
     response = requests.delete("http://127.0.0.1:8000/students/22")
